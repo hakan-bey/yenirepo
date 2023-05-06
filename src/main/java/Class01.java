bf2399e31c7b59bd777f694ee4134f8578495be6
hakan bey <hakangur1923@gmail.com>
2023-05-06 20:14:23 +0300

    version 2

---
+++ src/main/java/Class01.java
@@ -0,0 +1,14 @@
+public class Class01 {
+
+    public static void main(String[] args) {
+        int x =1;
+        int y =2;
+        System.out.println(x+y);
+        System.out.println("version 1 için işlemler tamam");
+        //version 2 çalışmaları
+        int z =4;
+        System.out.println(x+y+z);
+
+
+    }
+}
